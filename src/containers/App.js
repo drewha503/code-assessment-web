971b64c3caac53f58b1078ed6e5e137c2742dfaf
drewha503 <32880512+drewha503@users.noreply.github.com>
2018-04-29 16:51:14 -0700

    Add files via upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,8 +9,9 @@ const App = () => (
       <div className="itemscontainer">
       <div className="headerbox">
         <h2 className="header">Acme Store</h2>
-        <p className="storecopy"><img className="productcartimage" alt="cart" src={require('../Img/shopping_Cart_main.png')} />You cart is empty.</p>
-      </div><ProductsContainer /></div>
+        {/* <p className="storecopy"><img className="productcartimage" alt="cart" src={require('../Img/shopping_Cart_main.png')} /> You cart is empty.</p> */}
+      </div>
+      <ProductsContainer /></div>
         <hr/>
     <div className="cartcontainer"><CartContainer /></div>
   </div>
